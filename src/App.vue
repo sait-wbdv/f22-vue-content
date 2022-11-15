fc84bc98edeebbf1f39276b20d0967702d86759f
Ashlyn Knox <ashlyn@lilyx.io>
2022-11-15 14:49:09 -0700

    added code examples from today's class

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header class="bg-gradient-to-r from-violet-500 via-emerald-500 to-emerald-700 flex  justify-between items-center p-4 md:p-8">
-    <div class="flex gap-4 items-center">
-      <img alt="Vue logo" class="logo" src="@/assets/images/logo.svg" width="64" height="64" />
+  <header
+    class="flex items-center justify-between bg-gradient-to-r from-violet-500 via-emerald-500 to-emerald-700 p-4 md:p-8"
+  >
+    <div class="flex items-center gap-4">
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="@/assets/images/logo.svg"
+        width="64"
+        height="64"
+      />
       <p class="text-3xl font-bold text-slate-700">Lilyx</p>
     </div>
-      <nav>
-        <ul class="flex gap-4">
-          <li>
-           <RouterLink to="/" class="text-xl text-slate-50 font-semibold">Home</RouterLink>
-          </li>
-          <li>
-           <RouterLink to="/about" class="text-xl text-slate-50 font-semibold">About</RouterLink>
-          </li>
-        </ul>
-      </nav>
+    <nav>
+      <ul class="flex gap-4">
+        <li>
+          <RouterLink to="/" class="text-xl font-semibold text-slate-50"
+            >Home</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink to="/about" class="text-xl font-semibold text-slate-50"
+            >About</RouterLink
+          >
+        </li>
+      </ul>
+    </nav>
   </header>
   <!-- RouterView shows the content from your pages or "views" -->
   <RouterView />
-  <footer class="grid place-items-center bg-slate-700 text-white h-20">
+
+  <footer class="grid h-20 place-items-center bg-slate-700 text-white">
     <p>&copy; 2022 Ashlyn Knox</p>
   </footer>
 </template>
-
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,60 @@
 <script setup>
+const name = "augustus";
+
+const strCapitalize = (str) => {
+  return str[0] != str[0].toUpperCase()
+    ? str[0].toUpperCase() + str.slice(1)
+    : str;
+};
+
+const textColor = (str) => {
+  return str[0] != str[0].toUpperCase() ? "text-red-500" : "text-green-500";
+};
 </script>
 
 <template>
   <main>
-    <header class="flex justify-center items-center h-60">
-    <h1 class="py-4 text-2xl lg:text-5xl bg-clip-text text-transparent bg-gradient-to-br from-slate-900 to-emerald-500  font-extrabold">Home Page</h1>
+    <header class="flex h-60 flex-col items-center justify-center">
+      <h1
+        class="bg-gradient-to-br from-slate-900 to-emerald-500 bg-clip-text py-4 text-2xl font-extrabold text-transparent lg:text-5xl"
+      >
+        Home Page
+      </h1>
     </header>
+    <section
+      class="mx-8 mb-12 max-w-fit rounded-lg bg-slate-100 p-4 shadow-sm shadow-slate-900 lg:shadow-lg"
+    >
+      <header class="my-4">
+        <h2 class="text-2xl font-semibold text-slate-900 lg:text-5xl">
+          November 15 Examples
+        </h2>
+      </header>
+      <div class="py-2">
+        <h3 class="font-semibold">Render a Capitalized Name</h3>
+        <p class="max-w-prose">
+          In this example. The name is being capitalized using the function
+          strCapitalize(), and the text color is defined using a ternary
+          operatory in the paragraph tag's class binding
+        </p>
+        <div>
+          <p :class="textColor(name)" class="pb-2 text-xl lg:text-3xl">
+            {{ name }}
+          </p>
+          <p
+            :class="textColor(strCapitalize(name))"
+            class="pb-2 text-xl lg:text-3xl"
+          >
+            {{ strCapitalize(name) }}
+          </p>
+        </div>
+        <p class="text-sm text-slate-700">
+          <em
+            >By taking the reused ternary logic from the two paragraph tags to
+            determine color and making them a function, the template code is
+            much cleaner.</em
+          >
+        </p>
+      </div>
+    </section>
   </main>
 </template>
