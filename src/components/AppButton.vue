084da26ee3cfbe0e6573bbf191fcca658d0af4d2
Ashlyn Knox <ashlyn@lilyx.io>
2022-11-16 11:27:39 -0700

    props examples

---
+++ src/components/AppButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  text: String,
+  bgColor: {
+    type: String,
+    default: "bg-purple-500",
+  },
+
+  colors: {
+    type: Array,
+  },
+});
+</script>
+<template>
+  <button :class="bgColor" class="m-2 rounded-md px-4 py-2 text-white">
+    {{ text }}
+  </button>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,60 +1,35 @@
 <script setup>
+import AppCard from "../components/AppCard.vue";
+import AppButton from "../components/AppButton.vue";
 const name = "augustus";
 
-const strCapitalize = (str) => {
+function strCapitalize(str) {
   return str[0] != str[0].toUpperCase()
     ? str[0].toUpperCase() + str.slice(1)
     : str;
-};
+}
 
-const textColor = (str) => {
+function textColor(str) {
   return str[0] != str[0].toUpperCase() ? "text-red-500" : "text-green-500";
-};
+}
 </script>
 
 <template>
   <main>
     <header class="flex h-60 flex-col items-center justify-center">
       <h1
-        class="bg-gradient-to-br from-slate-900 to-emerald-500 bg-clip-text py-4 text-2xl font-extrabold text-transparent lg:text-5xl"
+        class="bg-gradient-to-br from-slate-900 to-emerald-500 bg-clip-text py-4 text-3xl font-extrabold text-transparent lg:text-5xl"
       >
         Home Page
       </h1>
     </header>
-    <section
-      class="container mx-8 mb-12 max-w-xl rounded-lg bg-slate-100 p-4 shadow-sm shadow-slate-900 lg:shadow-lg"
-    >
-      <header class="my-4">
-        <h2 class="text-2xl font-semibold text-slate-900 lg:text-5xl">
-          November 15 Examples
-        </h2>
-      </header>
-      <div class="py-2">
-        <h3 class="font-semibold">Render a Capitalized Name</h3>
-        <p class="max-w-prose">
-          In this example. The name is being capitalized using the function
-          strCapitalize(), and the text color is defined using a ternary
-          operatory in the paragraph tag's class binding
-        </p>
-        <div class="my-4">
-          <p :class="textColor(name)" class="pb-2 text-xl lg:text-3xl">
-            {{ name }}
-          </p>
-          <p
-            :class="textColor(strCapitalize(name))"
-            class="pb-2 text-xl lg:text-3xl"
-          >
-            {{ strCapitalize(name) }}
-          </p>
-        </div>
-        <p class="max-w-prose text-sm text-slate-700">
-          <em
-            >By taking the reused ternary logic from the two paragraph tags to
-            determine color and making them a function, the template code is
-            much cleaner.</em
-          >
-        </p>
-      </div>
+    <section class="flex justify-around">
+      <AppButton text="Push me" bgColor="bg-blue-500" />
+      <AppButton text="High Five" bgColor="bg-red-500" />
+      <AppButton text="Low Five" bgColor="bg-emerald-500" />
+    </section>
+    <section>
+      <AppCard />
     </section>
   </main>
 </template>
