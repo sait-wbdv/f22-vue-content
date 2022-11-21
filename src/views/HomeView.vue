cc98bf929fc74ada8ddc4f3b7392e55612699e14
Ashlyn Knox <ashlyn@lilyx.io>
2022-11-21 09:19:01 -0700

    simple v for loop

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,43 +4,61 @@ import AppButton from "../components/AppButton.vue";
 import AppHeader from "../components/AppHeader.vue";
 // JS Variable for youtube
 const info = {
-  title: "Welcome to the Home Page",
+  label: "Welcome to the Home Page",
   description:
     "This website is for documenting the things that we learn to do in VueJS.",
 };
-const youtube = {
-  src: "src/assets/images/youtube.svg",
-  alt: "Youtube Logo",
-};
+// const youtube = {
+//   src: "src/assets/images/youtube.svg",
+//   alt: "Youtube Logo",
+// };
+const buttons = [
+  {
+    text: "Push Me",
+    bgColor: "bg-blue-500",
+    src: "src/assets/images/logo.svg",
+  },
+  {
+    text: "High Five",
+    bgColor: "bg-red-500",
+    src: "src/assets/images/youtube.svg",
+  },
+  {
+    text: "Low FIve",
+    bgColor: "bg-emerald-500",
+  },
+];
+const arr = ["one", "two", "three", "four"];
 </script>
 
 <template>
   <main>
     <AppHeader
-      :title="info.title"
+      :title="info.label"
       :description="info.description"
-    />
+    >
+    </AppHeader>
     <section class="flex justify-around">
       <AppButton
-        text="Push me"
-        bgColor="bg-blue-500"
-        src="src/assets/images/logo.svg"
-      />
-      <AppButton
-        text="High Five"
-        bgColor="bg-red-500"
-        num="5"
-        :src="youtube.src"
-        :altText="youtube.alt"
-      />
-      <AppButton
-        text="Low Five"
-        bgColor="bg-emerald-500"
-        altText="Nothing"
+        v-for="thing in buttons"
+        :key="thing.id"
+        :text="thing.text"
+        :bgColor="thing.bgColor"
       />
     </section>
     <section>
       <AppCard />
+    </section>
+    <section>
+      <ul>
+        <!-- Attach v-for loop to the item that will be duplicated-->
+        <li
+          v-for="item in arr"
+          :key="item.id"
+        >
+          {{ item }}
+        </li>
+      </ul>
     </section>
   </main>
 </template>
